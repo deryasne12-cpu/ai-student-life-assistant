7c5e34b3f8ab684ee1941611c43fb7f4ea2d135f
ÖMER KESEN <oemer.kesen@ue-germany.de>
2026-05-04 01:08:28 +0200

    Update model.py

--- app/model.py
+++ app/model.py
@@ -1,1 +1,19 @@
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
 
+# Dummy training data
+X = np.array([
+    [4, -0.5],
+    [8, 0.6],
+    [6, 0.1],
+    [3, -0.8],
+    [7, 0.7]
+])
+
+y = ["Low", "High", "Medium", "Low", "High"]
+
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
+def predict_productivity(sleep, sentiment):
+    return model.predict([[sleep, sentiment]])[0]
